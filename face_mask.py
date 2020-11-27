a1015420f9401ef9a67c6ee65994e57ff2979d12
Phan Trinh <phan181299@gmail.com>
2020-11-27 01:16:04 -0500

    face_mask_model_training

---
+++ face_mask.py
@@ -0,0 +1,96 @@
+import os
+import os
+import zipfile
+import tensorflow as tf
+from tensorflow.python.keras.api._v1 import keras
+from tensorflow.python.keras.api._v1.keras import layers
+from tensorflow.python.keras.layers.core import Dropout
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import json
+import time
+
+train_dir = os.path.abspath('../train')
+validation_dir = os.path.abspath('../val')
+
+# Init model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(150, 150, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(3, activation='softmax')
+])
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy', 'categorical_accuracy'])
+# # augmented
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+    
+train_generator = train_datagen.flow_from_directory(
+        train_dir,  # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=20,
+        color_mode='grayscale',
+        class_mode='categorical')
+    
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=50,
+        color_mode='grayscale',
+        class_mode='categorical')
+    
+history = model.fit(
+      train_generator,
+      steps_per_epoch=20,  # 2000 images = batch_size * steps
+      epochs=50,
+      validation_data=validation_generator,
+      validation_steps=50,  # 1000 images = batch_size * steps
+      verbose=1)
+
+model.save(f'trained/model-{int(time.time())}')
+
+# Plotting
+
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+plt.plot(epochs, loss, 'r', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
